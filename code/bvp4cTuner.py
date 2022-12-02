f4f11b59037bb3c8cd52e6972165b105edd7b963
VictorVinySaajan <viny.saajan.victor@itwm.fraunhofer.de>
2022-12-02 16:57:29 +0100

    ml models, scalers and visualizer

--- code/bvp4cTuner.py
+++ code/bvp4cTuner.py
@@ -2,40 +2,49 @@ import numpy as np
 import pickle
 import pandas as pd
 import optuna
+from visualizer import Visualizer
+from optimizer import Optimizer
+
 
 class bvpFineTuner:
     """class that handles the fine-tuning of congiguration parameters of bvp4c solver from MATLAB"""
-    
+
     def __init__(self):
         self.optimizer = Optimizer()
-        
+        self.visualizer = Visualizer()
+
+    def setTrailNumber(self, trials):
+        self.optimizer.setTrailNumber(trials)
+
     def getSolvabiltyStatus(self, test_case_type, max_grid_points, newton_critical_tolerance,
-                             newton_armijo_probes, newton_max_iterations, newton_tolerance,
-                             add_factor, remove_factor, use_collocation_scaling):
+                            newton_armijo_probes, newton_max_iterations, newton_tolerance,
+                            add_factor, remove_factor, use_collocation_scaling):
         array = np.array([test_case_type, max_grid_points, np.log(newton_critical_tolerance),
-                             newton_armijo_probes, newton_max_iterations, np.log(newton_tolerance),
-                             add_factor, remove_factor, use_collocation_scaling])
+                          newton_armijo_probes, newton_max_iterations, np.log(newton_tolerance),
+                          add_factor, remove_factor, use_collocation_scaling])
         array = array.reshape(1, -1)
-        
+
         if bool(self.optimizer.getSolvabiltyStatus(array)):
             return 'The problem is solvable'
         else:
-            return 'The problem is not solvable' 
-    
+            return 'The problem is not solvable'
+
     def getSolverPerformance(self, test_case_type, max_grid_points, newton_critical_tolerance,
-                             newton_armijo_probes, newton_max_iterations, newton_tolerance, 
+                             newton_armijo_probes, newton_max_iterations, newton_tolerance,
                              add_factor, remove_factor, use_collocation_scaling):
         array = np.array([test_case_type, max_grid_points, np.log(newton_critical_tolerance),
                           newton_armijo_probes, newton_max_iterations, np.log(newton_tolerance),
                           add_factor, remove_factor, use_collocation_scaling])
         array = array.reshape(1, -1)
-        
+
         if bool(self.optimizer.getSolvabiltyStatus(array)):
             stats = self.optimizer.getSolverPerformance(array)
-            return print('\n','nODEevals: ', int(stats[0][0]), '\n','nGridPoints: ', int(stats[0][1]), '\n','maxResiduum: ', stats[0][2], '\n')
+            return print('\n', 'nODEevals: ', int(stats[0][0]), '\n', 'nGridPoints: ', int(stats[0][1]), '\n',
+                         'maxResiduum: ',
+                         stats[0][2], '\n')
         else:
             return 'The problem is not solvable'
-            
+
     def getOptimalODEevals(self, test_case_type):
         print("Calculating the best setting")
         self.optimizer.setTestCaseType(test_case_type)
@@ -62,52 +71,111 @@ class bvpFineTuner:
         best_trial.params['newton_tolerance'] = np.exp(best_trial.params['newton_tolerance'])
         print('Numerical Settings: ', best_trial.params)
         print('Maximum Residdum: ', best_trial.values)
-        
+
     def getOptimalODEevalsAndGridPoints(self, test_case_type):
         print("Calculating the pareto front settings")
         self.optimizer.setTestCaseType(test_case_type)
         best_trials = self.optimizer.twoCriteria('EvalGp')
+        row = pd.DataFrame(
+            columns=['test_case_type', 'max_grid_points', 'newton_critical_tolerance', 'newton_armijo_probes',
+                     'newton_max_iterations', 'newton_tolerance', 'add_factor', 'remove_factor',
+                     'use_collocation_scaling', 'ODE_Evaluations', 'Grid_Points'])
         for i in range(0, len(best_trials)):
-            print('\n')
-            best_trials[i].params['newton_critical_tolerance'] = np.exp(best_trials[i].params['newton_critical_tolerance'])
+            best_trials[i].params['newton_critical_tolerance'] = np.exp(
+                best_trials[i].params['newton_critical_tolerance'])
             best_trials[i].params['newton_tolerance'] = np.exp(best_trials[i].params['newton_tolerance'])
-            print('Numerical Settings: ', best_trials[i].params)
-            print('ODE Evaluations: ', int(best_trials[i].values[1]), 'Grid Points: ', int(best_trials[i].values[0]))
-            print('\n')
+            row.loc[i] = [test_case_type, best_trials[i].params['max_grid_points'],
+                          best_trials[i].params['newton_critical_tolerance'],
+                          best_trials[i].params['newton_armijo_probes'],
+                          best_trials[i].params['newton_max_iterations'], best_trials[i].params['newton_tolerance'],
+                          best_trials[i].params['add_factor'], best_trials[i].params['remove_factor'],
+                          best_trials[i].params['use_collocation_scaling'], best_trials[i].values[0],
+                          best_trials[i].values[1]]
+        print("Completed")
+        return row
 
     def getOptimalGridPointsAndResiduum(self, test_case_type):
         print("Calculating the pareto front settings")
         self.optimizer.setTestCaseType(test_case_type)
         best_trials = self.optimizer.twoCriteria('GpRes')
+        row = pd.DataFrame(
+            columns=['test_case_type', 'max_grid_points', 'newton_critical_tolerance', 'newton_armijo_probes',
+                     'newton_max_iterations', 'newton_tolerance', 'add_factor', 'remove_factor',
+                     'use_collocation_scaling', 'Grid_Points', 'Maximum_Residuum'])
         for i in range(0, len(best_trials)):
-            print('\n')
-            best_trials[i].params['newton_critical_tolerance'] = np.exp(best_trials[i].params['newton_critical_tolerance'])
+            best_trials[i].params['newton_critical_tolerance'] = np.exp(
+                best_trials[i].params['newton_critical_tolerance'])
             best_trials[i].params['newton_tolerance'] = np.exp(best_trials[i].params['newton_tolerance'])
-            print('Numerical Settings: ', best_trials[i].params)
-            print('Grid Points: ', int(best_trials[i].values[0]), 'Maximum Residuum: ', best_trials[i].values[1])
-            print('\n')
+            row.loc[i] = [test_case_type, best_trials[i].params['max_grid_points'],
+                          best_trials[i].params['newton_critical_tolerance'],
+                          best_trials[i].params['newton_armijo_probes'],
+                          best_trials[i].params['newton_max_iterations'], best_trials[i].params['newton_tolerance'],
+                          best_trials[i].params['add_factor'], best_trials[i].params['remove_factor'],
+                          best_trials[i].params['use_collocation_scaling'], best_trials[i].values[0],
+                          best_trials[i].values[1]]
+        print("Completed")
+        return row
 
     def getOptimalResiduumAndODEevals(self, test_case_type):
         print("Calculating the pareto front settings")
         self.optimizer.setTestCaseType(test_case_type)
         best_trials = self.optimizer.twoCriteria('ResEval')
+        row = pd.DataFrame(
+            columns=['test_case_type', 'max_grid_points', 'newton_critical_tolerance', 'newton_armijo_probes',
+                     'newton_max_iterations', 'newton_tolerance', 'add_factor', 'remove_factor',
+                     'use_collocation_scaling', 'Maximum_Residuum', 'ODE_Evaluations'])
         for i in range(0, len(best_trials)):
-            print('\n')
-            best_trials[i].params['newton_critical_tolerance'] = np.exp(best_trials[i].params['newton_critical_tolerance'])
+            best_trials[i].params['newton_critical_tolerance'] = np.exp(
+                best_trials[i].params['newton_critical_tolerance'])
             best_trials[i].params['newton_tolerance'] = np.exp(best_trials[i].params['newton_tolerance'])
-            print('Numerical Settings: ', best_trials[i].params)
-            print('Maximum Residuum: ', best_trials[i].values[0], 'ODE Evaluations: ', int(best_trials[i].values[1]))
-            print('\n')
+            row.loc[i] = [test_case_type, best_trials[i].params['max_grid_points'],
+                          best_trials[i].params['newton_critical_tolerance'],
+                          best_trials[i].params['newton_armijo_probes'],
+                          best_trials[i].params['newton_max_iterations'], best_trials[i].params['newton_tolerance'],
+                          best_trials[i].params['add_factor'], best_trials[i].params['remove_factor'],
+                          best_trials[i].params['use_collocation_scaling'], best_trials[i].values[0],
+                          best_trials[i].values[1]]
+        print("Completed")
+        return row
 
     def getOptimizedSettings(self, test_case_type):
         print("Calculating the pareto front settings")
         self.optimizer.setTestCaseType(test_case_type)
         best_trials = self.optimizer.threeCriteria()
+        row = pd.DataFrame(
+            columns=['test_case_type', 'max_grid_points', 'newton_critical_tolerance', 'newton_armijo_probes',
+                     'newton_max_iterations', 'newton_tolerance', 'add_factor', 'remove_factor',
+                     'use_collocation_scaling', 'ODE_Evaluations', 'Grid_Points', 'Maximum_Residuum'])
         for i in range(0, len(best_trials)):
-            print('\n')
-            best_trials[i].params['newton_critical_tolerance'] = np.exp(best_trials[i].params['newton_critical_tolerance'])
+            best_trials[i].params['newton_critical_tolerance'] = np.exp(
+                best_trials[i].params['newton_critical_tolerance'])
             best_trials[i].params['newton_tolerance'] = np.exp(best_trials[i].params['newton_tolerance'])
-            print('Numerical Settings: ', best_trials[i].params)
-            print('ODE Evaluations: ', int(best_trials[i].values[0]), 'Grid Points: ', int(best_trials[i].values[1]),
-                    'Maximum Residuum: ', best_trials[i].values[2])
-            print('\n')
+
+            row.loc[i] = [test_case_type, best_trials[i].params['max_grid_points'],
+                          best_trials[i].params['newton_critical_tolerance'],
+                          best_trials[i].params['newton_armijo_probes'],
+                          best_trials[i].params['newton_max_iterations'], best_trials[i].params['newton_tolerance'],
+                          best_trials[i].params['add_factor'], best_trials[i].params['remove_factor'],
+                          best_trials[i].params['use_collocation_scaling'], best_trials[i].values[0],
+                          best_trials[i].values[1],
+                          best_trials[i].values[2]]
+        print("Completed")
+        return row
+
+    def visualize(self, dataframe):
+        if len(dataframe.columns) == 11:
+            self.visualizer.visualize2d(dataframe)
+        elif len(dataframe.columns) == 12:
+            self.visualizer.visualize3d(dataframe)
+        else:
+            print('Dataframe is not in right format. Please provided the dataframe returned by bvpFineTuner functions')
+
+def main():
+    tuner = bvpFineTuner()
+    tuner.setTrailNumber(500)
+
+    optimizedSettings = tuner.getOptimalResiduumAndODEevals(test_case_type=8)
+    tuner.visualize(optimizedSettings)
+
+if __name__=='__main__':
+    main()
--- code/optimizer.py
+++ code/optimizer.py
@@ -3,31 +3,37 @@ import pickle
 import pandas as pd
 import optuna
 
+
 class Optimizer:
     """class for handling multi-objective optimization"""
-    
+
     def __init__(self):
         optuna.logging.disable_default_handler()
-        self.rgr = pickle.load(open('best_rgr.sav', 'rb'))
-        self.rgr_scaler = pickle.load(open('scaler_rgr.pkl', 'rb'))
-        self.clf = pickle.load(open('best_clf', 'rb'))
-        self.clf_scaler = pickle.load(open('scaler_clf.pkl', 'rb'))
+        self.rgr = pickle.load(open('../models/best_rgr.sav', 'rb'))
+        self.rgr_scaler = pickle.load(open('../models/scaler_rgr.pkl', 'rb'))
+        self.clf = pickle.load(open('../models/best_clf', 'rb'))
+        self.clf_scaler = pickle.load(open('../models/scaler_clf.pkl', 'rb'))
         self.test_case_type = None
-        
+        self.trials = 100
+
+    def setTrailNumber(self, trials):
+        self.trials = trials
+
     def setTestCaseType(self, tc_type):
         self.test_case_type = tc_type
-        
+
     def getSolvabiltyStatus(self, array):
         return self.clf.predict(self.clf_scaler.transform(array))
-    
+
     def getSolverPerformance(self, array):
         return self.rgr.predict(self.rgr_scaler.transform(array))
 
-    def predictions(self, test_case_type, max_grid_points, newton_critical_tolerance, newton_armijo_probes, newton_max_iterations,
+    def predictions(self, test_case_type, max_grid_points, newton_critical_tolerance, newton_armijo_probes,
+                    newton_max_iterations,
                     newton_tolerance, add_factor, remove_factor, use_collocation_scaling):
         array = np.array([test_case_type, max_grid_points, newton_critical_tolerance,
-                             newton_armijo_probes, newton_max_iterations, newton_tolerance,
-                             add_factor, remove_factor, use_collocation_scaling])
+                          newton_armijo_probes, newton_max_iterations, newton_tolerance,
+                          add_factor, remove_factor, use_collocation_scaling])
         array = array.reshape(1, -1)
 
         if self.getSolvabiltyStatus(array) == 0:
@@ -40,18 +46,18 @@ class Optimizer:
 
                   'max_grid_points': trial.suggest_int('max_grid_points', 100, 10000),
                   'newton_critical_tolerance': trial.suggest_float('newton_critical_tolerance',
-                                                                     np.log(1.0027016845009548e-12),
-                                                                     np.log(9.93227151144808e+307)),
+                                                                   np.log(1.0027016845009548e-12),
+                                                                   np.log(9.93227151144808e+307)),
                   'newton_armijo_probes': trial.suggest_int('newton_armijo_probes', 1, 10),
                   'newton_max_iterations': trial.suggest_int('newton_max_iterations', 1, 100),
                   'newton_tolerance': trial.suggest_float('newton_tolerance',
-                                                            np.log(1.0000700155834424e-12),
-                                                            np.log(0.0099994154229888)),
+                                                          np.log(1.0000700155834424e-12),
+                                                          np.log(0.0099994154229888)),
                   'add_factor': trial.suggest_float('add_factor', 1, 1000),
                   'remove_factor': trial.suggest_float('remove_factor', 1.5422041344544035e-06,
-                                                         1.9999960347604355),
+                                                       1.9999960347604355),
                   'use_collocation_scaling': trial.suggest_categorical('use_collocation_scaling', [0, 1])}
-        
+
         prediction = self.predictions(**params)
 
         return prediction[0][0], prediction[0][1], prediction[0][2]
@@ -71,7 +77,7 @@ class Optimizer:
     def optimize_ODEevals_gridPoints(self, trial):
         x, y, z = self.objective(trial)
         return x, y
-    
+
     def optimize_gridPoints_maxResiduum(self, trial):
         x, y, z = self.objective(trial)
         return y, z
@@ -83,27 +89,26 @@ class Optimizer:
     def singleCriterion(self, objective):
         study = optuna.create_study(directions=['minimize'])
         if objective == 'nODEevals':
-            study.optimize(self.optimizenODEevals, n_trials=50)
+            study.optimize(self.optimizenODEevals, n_trials=self.trials)
         elif objective == 'nGridPoints':
-            study.optimize(self.optimizenGridPoints, n_trials=50)
+            study.optimize(self.optimizenGridPoints, n_trials=self.trials)
         else:
-            study.optimize(self.optimizemaxResiduum,     n_trials=50)
+            study.optimize(self.optimizemaxResiduum, n_trials=self.trials)
 
         return study.best_trial
 
     def twoCriteria(self, objective):
         study = optuna.create_study(directions=['minimize', 'minimize'])
         if objective == 'EvalGp':
-            study.optimize(self.optimize_ODEevals_gridPoints, n_trials=50)
+            study.optimize(self.optimize_ODEevals_gridPoints, n_trials=self.trials)
         elif objective == 'GpRes':
-            study.optimize(self.optimize_gridPoints_maxResiduum, n_trials=50)
+            study.optimize(self.optimize_gridPoints_maxResiduum, n_trials=self.trials)
         else:
-            study.optimize(self.optimize_maxResiduum_ODEevals, n_trials=50)
+            study.optimize(self.optimize_maxResiduum_ODEevals, n_trials=self.trials)
 
         return study.best_trials
 
-
     def threeCriteria(self):
         study = optuna.create_study(directions=['minimize', 'minimize', 'minimize'])
-        study.optimize(self.objective, n_trials=50)
+        study.optimize(self.objective, n_trials=self.trials)
         return study.best_trials
